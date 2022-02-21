2c73c183dbbbc463dc35c8384d89bc4fbdebc169
13ruceYu <bruceyuhongbo@gmail.com>
2022-02-21 23:37:46 +0800

    study: java enum

---
+++ javasepro/app-oop22/src/com/bobo/d4_constant/Demo1.java
@@ -0,0 +1,9 @@
+package com.bobo.d4_constant;
+
+public class Demo1 {
+    public static final String USER_NAME = "bobo";
+
+    public static void main(String[] args) {
+        System.out.println(USER_NAME);
+    }
+}
---
+++ javasepro/app-oop22/src/com/bobo/d5_enum/Demo1.java
@@ -0,0 +1,9 @@
+package com.bobo.d5_enum;
+
+/**
+ * 枚举类
+ */
+public enum Demo1 {
+    // 第一行罗列枚举类的对象名称，建议全部大写
+    SPRING, SUMMER, AUTUMN, WINTER;
+}
