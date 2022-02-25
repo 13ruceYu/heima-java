44b32f1f4583199d852857c4794c31981750d3bb
13ruceYu <bruceyuhongbo@gmail.com>
2022-02-25 23:20:55 +0800

    moyu

---
+++ javasepro/app-oop22/src/com/bobo/d10_interface/SportsmanInterface.java
@@ -0,0 +1,12 @@
+package com.bobo.d10_interface;
+
+public interface SportsmanInterface {
+    // 接口成员：JDK 1.8 之前只有常量 & 抽象方法
+    // public static final 可以省略不写
+    // public static final String DEFAULT_NAME = "BOBO";
+    String DEFAULT_NAME = "BOBO";
+    String[] NAME_LIST = {"whh", "zks"};
+
+    // 抽象方法 public abstract 可以省略不写
+    void run();
+}
