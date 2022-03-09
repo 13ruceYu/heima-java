a81d27382bac78e05eed6004e3553198dca7c38c
13ruceYu <bruceyuhongbo@gmail.com>
2022-03-09 23:15:43 +0800

    study: java math

---
+++ javasepro/app-oop3/src/com/bobo/d12_math/Demo.java
@@ -0,0 +1,9 @@
+package com.bobo.d12_math;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a = Math.abs(-1);
+        double b = Math.floor(11.11);
+        double c = Math.random();
+    }
+}
---
+++ javasepro/app-oop3/src/com/bobo/d13_system/Demo.java
@@ -0,0 +1,11 @@
+package com.bobo.d13_system;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("start");
+
+        System.exit(0);
+
+        System.out.println("end");
+    }
+}
