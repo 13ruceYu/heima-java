eec7bebd021b98bf0e8b4c99e551cc9199bdb3ec
13ruceYu <bruceyuhongbo@gmail.com>
2022-03-17 23:58:50 +0800

    study: java 自动装箱

---
+++ javasepro/app-api-lambda/src/com/bobo/d5_integer/Demo.java
@@ -0,0 +1,15 @@
+package com.bobo.d5_integer;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer a1 = 11;
+        Integer a2 = a; // 自动装箱
+        System.out.println(a);
+        System.out.println(a1);
+
+        Integer it = 100;
+        int it1 = it; // 自动拆箱
+        System.out.println(it);
+    }
+}
