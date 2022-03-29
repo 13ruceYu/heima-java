31c13935a406b4a2a668a314095d80979b7bf736
13ruceYu <bruceyuhongbo@gmail.com>
2022-03-29 23:53:35 +0800

    study: java connect mysql

---
+++ jdbc/jdbc-demo/src/com/bobo/jdbc/JDBCDemo.java
@@ -0,0 +1,10 @@
+package com.bobo.jdbc;
+
+/**
+ * JDBC 快速入门
+ */
+public class JDBCDemo {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+}
