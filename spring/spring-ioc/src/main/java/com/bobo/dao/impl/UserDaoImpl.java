879f2b14e1920a267e63a37a12ac646b86056da6
13ruceYu <bruceyuhongbo@gmail.com>
2022-04-18 23:54:17 +0800

    study: spring config data inject

--- spring/spring-ioc/src/main/java/com/bobo/dao/impl/UserDaoImpl.java
+++ spring/spring-ioc/src/main/java/com/bobo/dao/impl/UserDaoImpl.java
@@ -1,22 +1,80 @@
 package com.bobo.dao.impl;
 
 import com.bobo.dao.UserDao;
+import com.bobo.domain.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class UserDaoImpl implements UserDao {
-    public UserDaoImpl() {
-        System.out.println("UserDaoImpl 创建...");
+
+    private String username;
+    private int age;
+
+    private List<String> stringList;
+    private Map<String, User> userMap;
+    private Properties properties;
+
+    public List<String> getStringList() {
+        return stringList;
     }
 
-    public void init() {
-        System.out.println("impl init...");
+    public void setStringList(List<String> stringList) {
+        this.stringList = stringList;
     }
 
-    public void destroy() {
-        System.out.println("impl destroy...");
+    public Map<String, User> getUserMap() {
+        return userMap;
     }
+
+    public void setUserMap(Map<String, User> userMap) {
+        this.userMap = userMap;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // public UserDaoImpl() {
+    //     System.out.println("UserDao 创建...");
+    // }
+    //
+    // public void init() {
+    //     System.out.println("userDao init...");
+    // }
+    //
+    // public void destroy() {
+    //     System.out.println("userDao destroy...");
+    // }
 
     @Override
     public void save() {
-        System.out.println("user dao impl...");
+        // System.out.println(username + "===" + age);
+        System.out.println(stringList);
+        System.out.println(userMap);
+        System.out.println(properties);
+        System.out.println("userDao save...");
+
     }
 }
