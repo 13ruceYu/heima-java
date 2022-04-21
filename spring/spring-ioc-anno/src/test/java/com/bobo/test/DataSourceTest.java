5efcef618fbb8f750ed8ac443258ad9ea9afe25e
13ruceYu <bruceyuhongbo@gmail.com>
2022-04-21 22:56:54 +0800

    study: jdbc config

--- spring/spring-ioc-anno/src/test/java/com/bobo/test/DataSourceTest.java
+++ spring/spring-ioc-anno/src/test/java/com/bobo/test/DataSourceTest.java
@@ -1,18 +1,48 @@
 package com.bobo.test;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.util.ResourceBundle;
 
 public class DataSourceTest {
+
+    @Test
+    // test c3p0 datasource(load property file)
+    public void test3() throws Exception {
+        // read property file
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("jdbc");
+        String driver = resourceBundle.getString("jdbc.driver");
+        String url = resourceBundle.getString("jdbc.url");
+        String username = resourceBundle.getString("jdbc.username");
+        String password = resourceBundle.getString("jdbc.password");
+
+        // create datasource
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setDriverClass(driver);
+        comboPooledDataSource.setJdbcUrl(url);
+        comboPooledDataSource.setUser(username);
+        comboPooledDataSource.setPassword(password);
+
+        Connection connection = comboPooledDataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
 
     @Test
     // test druid datasource
     public void test2() throws Exception {
         DruidDataSource druidDataSource = new DruidDataSource();
         druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        druidDataSource.setUrl("jdbc:mysql://localhost:3306/db1");
+        druidDataSource.setUsername("root");
+        druidDataSource.setPassword("892400jj");
+        DruidPooledConnection connection = druidDataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
     @Test
