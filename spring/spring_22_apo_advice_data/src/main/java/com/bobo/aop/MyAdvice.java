2d6c88c02a74a8c2fa953198963cfda365f30322
13ruceYu <bruceyuhongbo@gmail.com>
2022-05-19 23:25:36 +0800

    spring: aop advice data

--- spring/spring_22_apo_advice_data/src/main/java/com/bobo/aop/MyAdvice.java
+++ spring/spring_22_apo_advice_data/src/main/java/com/bobo/aop/MyAdvice.java
@@ -1,9 +1,8 @@
 package com.bobo.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -15,10 +14,36 @@ public class MyAdvice {
     @Pointcut("execution(* com.bobo.dao.BookDao.findName(..))")
     private void pt() {}
 
-    @Before("pt()")
+    // @Before("pt()")
     public void before(JoinPoint jp) {
         Object[] args = jp.getArgs();
         System.out.println(Arrays.toString(args));
         System.out.println("before advice...");
+    }
+
+    // @After("pt()")
+    public void after(JoinPoint jp) {
+        Object[] args = jp.getArgs();
+        System.out.println(Arrays.toString(args));
+        System.out.println("before advice...");
+    }
+
+    // @Around("pt()")
+    public Object around(ProceedingJoinPoint pjp)  {
+        Object[] args = pjp.getArgs();
+        System.out.println(Arrays.toString(args));
+        args[0] = 888;
+        Object proceed = null;
+        try {
+            proceed = pjp.proceed(args);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return proceed;
+    }
+
+    @AfterReturning(value = "pt()", returning = "ret")
+    public void afterReturning(Object ret) {
+        System.out.println("afterReturning advice..." + ret);
     }
 }
