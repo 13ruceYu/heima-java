07b73cda8c657501b5bfbe999b1faf94a9d3854c
13ruceYu <bruceyuhongbo@gmail.com>
2022-05-25 23:29:26 +0800

    springmvc rest case

---
+++ springmvc/springmvc_07_rest_case/src/main/java/com/bobo/config/SpringMvcSupport.java
@@ -0,0 +1,6 @@
+package com.bobo.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class SpringMvcSupport extends WebMvcConfigurationSupport {
+}
--- springmvc/springmvc_07_rest_case/src/main/java/com/bobo/controller/BookController.java
+++ springmvc/springmvc_07_rest_case/src/main/java/com/bobo/controller/BookController.java
@@ -1,4 +1,38 @@
 package com.bobo.controller;
 
+import com.bobo.domain.Book;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    @PostMapping
+    public String save(@RequestBody Book book) {
+        System.out.println("book save ===>" + book);
+        return "book save success";
+    }
+
+    @GetMapping
+    public List<Book> getAll() {
+
+        List<Book> bookList = new ArrayList<Book>();
+
+        Book book1 = new Book();
+        book1.setName("the good part of JS");
+        book1.setType("CS");
+        book1.setDescription("JS language");
+        bookList.add(book1);
+
+        Book book2 = new Book();
+        book2.setName("Java Yo");
+        book2.setType("CS");
+        book2.setDescription("yoooo");
+        bookList.add(book2);
+
+        return bookList;
+    }
 }
