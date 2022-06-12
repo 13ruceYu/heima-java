3637b26aa2ddf13cc0d3debc6e59d03f8a4108bd
13ruceYu <bruceyuhongbo@gmail.com>
2022-06-12 23:23:30 +0800

    springboot mybatis

---
+++ springboot/springboot_08_mybatis/src/test/java/com/bobo/Springboot08MybatisApplicationTests.java
@@ -0,0 +1,21 @@
+package com.bobo;
+
+import com.bobo.dao.BookDao;
+import com.bobo.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot08MybatisApplicationTests {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void testGetById() {
+        Book book = bookDao.getById(1);
+        System.out.println(book);
+    }
+
+}
