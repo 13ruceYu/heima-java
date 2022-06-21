c29b775659b155607672e8c0cd0f46ac7d4488c9
13ruceYu <bruceyuhongbo@gmail.com>
2022-06-21 23:57:32 +0800

    reggie employee login

--- projects/reggie_take_out/src/main/java/com/bobo/reggie/controller/EmployeeController.java
+++ projects/reggie_take_out/src/main/java/com/bobo/reggie/controller/EmployeeController.java
@@ -1,12 +1,32 @@
 package com.bobo.reggie.controller;
 
+import com.bobo.reggie.common.R;
+import com.bobo.reggie.entity.Employee;
+import com.bobo.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        return null;
+    }
 
 }
